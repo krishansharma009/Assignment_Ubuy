0beb2befef3f58be21339210e0424b2750907de0
krishansharma009 <godkrihna.apple.mtt@gmail.com>
2024-12-16 13:05:34 +0530

    new some chnge

--- app/page.js
+++ app/page.js
@@ -45,17 +45,20 @@ export default function Home() {
 
     if (recaptchaValue) {
       try {
-        const response = await fetch("http://localhost:8080/users/register", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email,
-            password,
-            otp,
-          }),
-        });
+        const response = await fetch(
+          "https://assignment-newubuybackend.onrender.com/users/register",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email,
+              password,
+              otp,
+            }),
+          }
+        );
 
         const data = await response.json();
 
@@ -80,16 +83,19 @@ export default function Home() {
     e.preventDefault();
     if (recaptchaValue) {
       try {
-        const response = await fetch("http://localhost:8080/users/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email,
-            password,
-          }),
-        });
+        const response = await fetch(
+          "https://assignment-newubuybackend.onrender.com/users/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email,
+              password,
+            }),
+          }
+        );
 
         const data = await response.json();
 
